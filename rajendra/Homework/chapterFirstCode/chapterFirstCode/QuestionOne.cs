4292b74c24320980090a74a315a7d3b13b930699
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-19 19:37:52 +0545

    Question 1 Solution

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionOne
+    {
+
+        public void swap()
+        {
+            int i, j;
+
+            Console.Write($"Enter first Number : ");
+            i = int.Parse(Console.ReadLine());
+
+            Console.Write($"Enter second Number : ");
+            j = int.Parse(Console.ReadLine());
+
+            int temp = i;
+            i = j;
+            j = temp;
+
+            Console.WriteLine($"Original Value [{i} , {j}] ");
+            Console.WriteLine($"After Swap Value [{i} , {j}] ");
+
+        }
+    }
+}
