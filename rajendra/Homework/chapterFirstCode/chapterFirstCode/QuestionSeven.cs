4d14ad2bd038758f05aadb748d81eb426f3f88a9
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 14:46:47 +0545

    Question No 7

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionSeven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionSeven
+    {
+        public void seven()
+        {
+            Console.Write($"Enter Temperature in Fahrenheit : ");
+            int f = int.Parse(Console.ReadLine());
+
+            double c = (f - 32) / 1.8;
+
+            Console.WriteLine($"The [{f} deg] is equal to [{c} Cel]");
+        }
+    }
+}
