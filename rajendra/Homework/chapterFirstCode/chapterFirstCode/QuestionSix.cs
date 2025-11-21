fba41c8ed84a312851448d330052433e70886237
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 14:32:43 +0545

    Question No 6

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionSix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionSix
+    {
+
+        public void six()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int originalNum = num;
+            int digits = 0;
+            int temp = num;
+
+            // Count digits
+            while (temp != 0)
+            {
+                temp /= 10;
+                digits++;
+            }
+
+            temp = num;
+            double result = 0;
+
+
+            while (temp != 0)
+            {
+                int remainder = temp % 10;
+                result += Math.Pow(remainder, digits);
+                temp /= 10;
+            }
+
+            if (result == originalNum)
+                Console.WriteLine($"{originalNum} is an Armstrong number.");
+            else
+                Console.WriteLine($"{originalNum} is not an Armstrong number.");
+        }
+    }
+}
