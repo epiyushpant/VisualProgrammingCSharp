b40357e76d119e31806622066d4f1641098cd317
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 15:13:19 +0545

    Question No 11

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionEleven.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionEleven
+    {
+        public void eleven()
+        {
+            Console.Write("Enter value of N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[N - 1];
+            Console.WriteLine($"Enter {N - 1} numbers between 1 and {N}:");
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int expectedSum = N * (N + 1) / 2;
+
+
+            int actualSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                actualSum += arr[i];
+            }
+
+            int missingNumber = expectedSum - actualSum;
+            Console.WriteLine("Missing number is: " + missingNumber);
+        }
+    }
+}
