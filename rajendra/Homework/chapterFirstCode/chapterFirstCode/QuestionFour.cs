ee7f2c4f8bea9707e1d14fd5073b091a79168dce
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 15:23:46 +0545

    Question No 4

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionFour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionFour
+    {
+        static void four()
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out long number))
+            {
+                string reversed = ReverseString(input);
+
+                if (input == reversed)
+                {
+                    Console.WriteLine($"{number} is a palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is not a palindrome.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
