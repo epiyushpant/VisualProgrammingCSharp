195ff996cf2b15284238e05b746674503760be64
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 15:07:06 +0545

    Question No 10

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionTen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionTen
+    {
+        public void ten()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine().ToLower();
+
+            int count = 0;
+            foreach (char ch in input)
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Number of vowels: " + count);
+        }
+    }
+}
