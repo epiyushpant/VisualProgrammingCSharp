46c50c2115ec8602192d48777326d3ba959b6009
rajendra-chimala <rajendrachimala5@gmail.com>
2025-11-21 14:25:50 +0545

    Question No 5

---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionFive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionFive
+    {
+
+
+        public void five()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int reversed = 0;
+
+            while (num != 0)
+            {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+
+            Console.WriteLine($"Reversed number  {reversed}");
+        }
+    }
+}
---
+++ rajendra/Homework/chapterFirstCode/chapterFirstCode/QuestionThree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterFirstCode
+{
+    internal class QuestionThree
+    {
+
+        public void prime()
+        {
+            int i;
+
+            Console.Write($"Enter a Number to Check Prime of Not : ");
+            i = int.Parse(Console.ReadLine());
+
+            if (i <= 1)
+                Console.WriteLine($" [{i}] is Not Prime Number !");
+            else
+            {
+                for (int x = 2; x <= i * i; x++)  
+                {
+                    if (i % x == 0)
+                    {
+
+                        Console.WriteLine($" [{i}] is Not Prime Number !");
+
+                        break;
+                        
+                    }
+                }
+
+                Console.WriteLine($" [{i}] is Prime Number !");
+
+            }
+
+
+
+
+        }
+    }
+}
