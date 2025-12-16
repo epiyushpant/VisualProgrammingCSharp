ffed805d3ccb0f968f1fa72e25f97684a2afa2d9
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 20:11:29 +0545

    [Chapter 3] Question No 2 Solution

---
+++ rajendra/Homework/chapterThirdCode/chapterThirdCode/QuestionTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterThirdCode
+{
+    internal class QuestionTwo
+    {
+        public void two()
+        {
+            List<string> words = new List<string> { "apple", "banana", "dog", "strawberry", "cat" };
+
+           
+            var longWords = from word in words
+                            where word.Length > 5
+                            select word;
+
+            Console.WriteLine("Strings longer than 5 characters (query syntax):");
+            foreach (var w in longWords)
+            {
+                Console.WriteLine(w);
+            }
+        }
+    }
+}
