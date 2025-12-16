1574b61abd0dc85ad0eb96caab9b3574272237af
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 16:03:25 +0545

    [Chapter 2] Question No 4 Solution

---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/QuestionFour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterSecondCode
+{
+    internal class QuestionFour
+    {
+        public void four()
+        {
+            List<int> data = new List<int> { 1, 2, -3, 4 };
+
+            bool anyNegativeNumber = data.Any(n => n < 0);
+
+            Console.WriteLine(anyNegativeNumber);
+
+        }
+    }
+}
