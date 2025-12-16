ca30662122e8d4ce3c679365085261b45368d56e
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 16:07:44 +0545

    [Chapter 2] Question No 6 Solution

---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/QuestionSix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterSecondCode
+{
+    internal class QuestionSix
+    {
+        public void six()
+        {
+            Func<string, bool> startsWithA = s => s.StartsWith("A");
+
+            Console.WriteLine(startsWithA("Apple"));   
+            Console.WriteLine(startsWithA("Banana"));  
+
+        }
+    }
+}
