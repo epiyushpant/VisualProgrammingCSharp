625dc1af0b47acda634a91b39f2bd180f01ba33e
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 15:52:04 +0545

    [Chapter 2] Question No 1 Solution

---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chapterSecondCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/QuestionOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterSecondCode
+{
+    internal class QuestionOne
+    {
+        public void one () {
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+
+            string reversed = "";
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + userInput[i];
+            }
+
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+    }
+}
