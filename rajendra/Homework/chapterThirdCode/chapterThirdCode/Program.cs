6e30dbad1ef3b86c554399a766589ea92351b68c
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 20:06:30 +0545

    [Chapter 3] Question No 1 Solution

---
+++ rajendra/Homework/chapterThirdCode/chapterThirdCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chapterThirdCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ rajendra/Homework/chapterThirdCode/chapterThirdCode/QuestionOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterThirdCode
+{
+    internal class QuestionOne
+    {
+        public void one()
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+            // Using LINQ query syntax
+            var firstThree = (from num in numbers
+                              select num).Take(3);
+
+            Console.WriteLine("First 3 elements:");
+            foreach (var n in firstThree)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
