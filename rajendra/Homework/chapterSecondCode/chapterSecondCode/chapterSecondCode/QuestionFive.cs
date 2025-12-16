bfd4a68a3beed2778501ed58bbee6701b2949646
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 16:05:31 +0545

    [Chapter 2] Question No 5 Solution

---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/QuestionFive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterSecondCode
+{
+    internal class QuestionFive
+    {
+        public void five()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+            foreach (var n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
