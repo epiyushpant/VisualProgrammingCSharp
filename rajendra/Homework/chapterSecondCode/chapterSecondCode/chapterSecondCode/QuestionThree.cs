2a361c3a94c679d2044ad6c5eab082cd9aabaf09
rajendra-chimala <rajendrachimala5@gmail.com>
2025-12-16 16:00:15 +0545

    [Chapter 2] Question No 3 Solution

---
+++ rajendra/Homework/chapterSecondCode/chapterSecondCode/chapterSecondCode/QuestionThree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapterSecondCode
+{
+    internal class QuestionThree
+    {
+        public void three()
+        {
+            List<string> words = new List<string> { "apple", "dog", "banana" };
+
+            words.Sort((a, b) => a.Length.CompareTo(b.Length));
+
+        }
+    }
+}
